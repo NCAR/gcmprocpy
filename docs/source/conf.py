16c0e1676feeb245f550fc99eaab85c5354fe4a5
Nikhil Rao <44057004+AnonNick@users.noreply.github.com>
2024-06-27 22:42:32 -0400

    Removed module list in function names

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 autodoc_mock_imports = ["numpy","cartopy","matplotlib","xarray","ipython","geomag"]
-
+add_module_names = False
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
