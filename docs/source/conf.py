54b7883fc7d0cda14728e9ed04cd436415688e60
Nikhil Rao <44057004+AnonNick@users.noreply.github.com>
2024-12-03 01:26:51 +0530

    Update docs conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode'
 templates_path = ['_templates']
 exclude_patterns = []
 
-autodoc_mock_imports = ["numpy","cartopy","matplotlib","xarray","ipython","geomag"]
+autodoc_mock_imports = ["numpy","cartopy","matplotlib","xarray","ipython","geomag","netcdf4","ipympl","mplcursors"]
 
 
 # -- Options for HTML output -------------------------------------------------
